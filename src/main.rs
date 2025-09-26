14692b4137a045076250ce52041d3665468e06bb
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-09-26 15:16:08 +0900

    feat: implemented wine iso run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,149 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use nanoid::nanoid;
+use std::{
+    collections::{HashMap, HashSet},
+    ffi::OsStr,
+    fs::{self, File},
+    io::{Read, Write},
+    path::{Path, PathBuf},
+    process::{Command, ExitStatus, Stdio},
+};
+
+use anyhow::{Result, anyhow, bail};
+use directories::ProjectDirs;
+use serde::{Deserialize, Serialize};
+const APP_NAME: &str = env!("CARGO_PKG_NAME");
+const WINEPREFIX: &str = "WINEPREFIX";
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    data_dir: Option<PathBuf>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PathMap {
+    path_map: HashMap<PathBuf, String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExecEnv {
+    executed_tricks: HashSet<String>,
+}
+
+#[derive(Parser, Debug)]
+#[command(version,about,long_about = None)]
+struct Args {
+    #[arg(long)]
+    with_tricks: Vec<String>,
+    exec_path: PathBuf,
+    args: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    run(Args::parse())
+}
+
+fn run(args: Args) -> Result<()> {
+    let data_dir = prepare()?;
+    let path_map_path = data_dir.join("path_map.toml");
+    let mut path_map_file = if path_map_path.exists() {
+        File::open(path_map_path)?
+    } else {
+        File::create_new(path_map_path)?
+    };
+    let mut path_map_data = vec![];
+    path_map_file.read_to_end(&mut path_map_data)?;
+    let mut path_map = toml::from_slice::<PathMap>(&path_map_data)?;
+    let id = if let Some(id) = path_map.path_map.get(&args.exec_path) {
+        id.clone()
+    } else {
+        let id = nanoid!();
+        path_map.path_map.insert(args.exec_path.clone(), id.clone());
+        path_map_file.write_all(toml::to_string_pretty(&path_map)?.as_bytes())?;
+        id
+    };
+    let exec_env_path = data_dir.join(id);
+    if !exec_env_path.exists() {
+        fs::create_dir_all(&exec_env_path)?;
+    }
+
+    let exec_env_conf_path = exec_env_path.join("conf.toml");
+    let mut exec_env_conf_file = if exec_env_conf_path.exists() {
+        File::open(&exec_env_conf_path)?
+    } else {
+        File::create_new(&exec_env_conf_path)?
+    };
+    let mut exec_env_conf_buf = vec![];
+    exec_env_conf_file.read_to_end(&mut exec_env_conf_buf)?;
+    let mut exec_conf = toml::from_slice::<ExecEnv>(&exec_env_conf_buf)?;
+    let exec_env_wine_path = exec_env_path.join(".wine");
+    if !exec_env_wine_path.exists() {
+        fs::create_dir_all(&exec_env_wine_path)?;
+    }
+
+    println!("Resolve winetricks...");
+    for trick in args.with_tricks {
+        if !exec_conf.executed_tricks.contains(&trick) {
+            exec_conf.executed_tricks.insert(trick.clone());
+            let status = exec_command("winetricks", &[trick.to_string()], &exec_env_wine_path)?;
+            if !status.success() {
+                bail!("winetricks is not succeed {trick}, status:{status}");
+            }
+            exec_env_conf_file.write_all(toml::to_string_pretty(&exec_conf)?.as_bytes())?;
+        }
+    }
+    let exec_path_str = args.exec_path.to_string_lossy().to_string();
+    let mut wine_args = vec![exec_path_str.clone()];
+    wine_args.extend_from_slice(&args.args);
+    println!("Run wine {exec_path_str}");
+    let status = exec_command("wine", wine_args, exec_env_wine_path)?;
+    if !status.success() {
+        bail!("wine is not succeed {status}");
+    }
+
+    Ok(())
+}
+fn exec_command<I, S>(
+    command: impl AsRef<str>,
+    args: I,
+    wine_prefix: impl AsRef<Path>,
+) -> Result<ExitStatus>
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    Ok(Command::new(command.as_ref())
+        .args(args)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .env(WINEPREFIX, wine_prefix.as_ref().as_os_str())
+        .status()?)
+}
+fn prepare() -> Result<PathBuf> {
+    if let Some(project_dirs) = ProjectDirs::from("", "", APP_NAME) {
+        if !project_dirs.data_dir().exists() {
+            fs::create_dir_all(project_dirs.data_dir())?;
+        }
+        let conf_path = project_dirs.data_dir().join("config.toml");
+        let mut conf_file = if !conf_path.exists() {
+            File::create_new(conf_path)?
+        } else {
+            File::open(conf_path)?
+        };
+        let mut conf_data = vec![];
+        conf_file.read_to_end(&mut conf_data)?;
+        let mut conf = toml::from_slice::<Config>(&conf_data)?;
+        if conf.data_dir.is_none() {
+            conf.data_dir = Some(project_dirs.data_local_dir().to_path_buf());
+            let save_data = toml::to_string_pretty(&conf)?;
+            conf_file.write_all(save_data.as_bytes())?;
+        }
+        let data_dir = conf.data_dir.unwrap();
+        if !data_dir.exists() {
+            fs::create_dir_all(&data_dir)?;
+        }
+        Ok(data_dir)
+    } else {
+        Err(anyhow!("Can not create project dir."))
+    }
 }
